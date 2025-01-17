713c1ba040206bf35d6eb6a6ace9d449a1d480a2
StepanRiazhkin <grrrabe@gmail.com>
2025-01-17 11:59:41 -0800

    Refined the project

--- src/test/java/com/crm/utilites/Driver.java
+++ src/test/java/com/crm/utilites/Driver.java
@@ -3,20 +3,26 @@ package com.crm.utilites;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
 import java.time.Duration;
 
 public class Driver {
 
-    private Driver(){};
+    private Driver() {
+    }
 
     public static final InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if(driverPool.get() == null){
+        String gridAddress;
+
+        if (driverPool.get() == null) {
 
             String browser;
             if (System.getProperty("browser") != null) {
@@ -25,41 +31,63 @@ public class Driver {
                 browser = ConfigReader.getProperty("browser");
             }
 
-            switch (browser){
+            switch (browser) {
 
-                case "remote-browser":
-                    try{
+                case "remote-chrome":
+                    try {
 
-                        URL url = new URL("http://174.129.57.20:4444/wd/hub");
+                        gridAddress = "100.29.38.166";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.addArguments("--start-maximized");
+                        driverPool.set(new RemoteWebDriver(url, chromeOptions));
 
 
-                    }catch (Exception e){
-
+                    } catch (Exception e) {
                         e.printStackTrace();
-
                     }
                     break;
+
+                case "remote-firefox":
+
+                    try {
+
+                        gridAddress = "54.162.50.13";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        firefoxOptions.addArguments("--start-maximized");
+                        driverPool.set(new RemoteWebDriver(url, firefoxOptions));
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
                 case "chrome":
                     driverPool.set(new ChromeDriver());
-                    driverPool.get().manage().window().maximize();
-                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
+
+                case "headless-chrome":
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless=new");
+                    driverPool.set(new ChromeDriver(options));
+                    break;
+
                 case "firefox":
                     driverPool.set(new FirefoxDriver());
-                    driverPool.get().manage().window().maximize();
-                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+
                 default:
                     System.err.println("No such driver type provided in Driver class");
                     throw new IllegalArgumentException("available types: chrome, firefox, remote-browser");
             }
 
-            return driverPool.get();
-
-        }else{
-
-            return driverPool.get();
+            driverPool.get().manage().window().maximize();
+            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         }
+
+        return driverPool.get();
 
     }
 
